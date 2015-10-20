15adcec718c480cc2591385dea1a00515bbaa7d6
stuetzle <stuetzle@ulb.ac.be>
2015-10-20 22:00:21 +0000

    update

---
+++ bibliography.sh
@@ -0,0 +1,7 @@
+ln -s ~/Papers/BIB/abbrev.bib abbrev.bib
+ln -s ~/Papers/BIB/journals.bib journals.bib
+ln -s ~/Papers/BIB/authors.bib authors.bib
+ln -s ~/Papers/BIB/biblio.bib biblio.bib
+ln -s ~/Papers/BIB/crossref.bib crossref.bib
+ln -s ~/Papers/BIB/abbrevshort.bib abbrevshort.bib
+
