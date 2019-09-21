7daa157bea3882a6a9094a7578cb695ec707c131
MLopez-Ibanez <2620021+MLopez-Ibanez@users.noreply.github.com>
2019-09-21 20:11:09 +0100

    travis_deploy.sh: Fix GIT_USER -> GIT_NAME

--- test/travis_deploy.sh
+++ test/travis_deploy.sh
@@ -2,13 +2,13 @@
 set -u
 set -e
 MESSAGE=$(git log --format=%B -n 1 $TRAVIS_COMMIT)
-git config --global user.name "${GIT_USER}"
+git config --global user.name "${GIT_NAME}"
 git config --global user.email "${GIT_EMAIL}"
 #git checkout -b master
 git add testbib.pdf
 git st
-git commit --message "testbib.pdf: Regenerate."
+git commit --message "testbib.pdf: Regenerate (build: $TRAVIS_BUILD_NUMBER)."
 #git remote add origin-pages https://${GH_TOKEN}@github.com/MVSE-outreach/resources.git > /dev/null 2>&1
 #git push --quiet --set-upstream origin gh-pages
 git push "https://${GH_TOKEN}@${GH_REPO}" master
-git config --global --list
+
