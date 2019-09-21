95c27c830cc9735900075c78a48edde0eb1d22af
MLopez-Ibanez <2620021+MLopez-Ibanez@users.noreply.github.com>
2019-09-21 22:37:16 +0100

    test/travis_maybe_skip.sh: Skip travis if not bib or tex files.

---
+++ test/travis_maybe_skip.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -u
+
+INCLUDE_ONLY="\.bib\|\.tex"
+
+git diff-tree --no-commit-id --name-only -r ${TRAVIS_COMMIT} | grep -e ${INCLUDE_ONLY} --quiet
+if [ $? -ne 0 ]; then
+    travis_terminate 0
+    exit 1
+fi
+exit 0
