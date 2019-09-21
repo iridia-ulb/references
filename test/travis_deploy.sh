c537df3e7b26fb59d0c2a9f60cafa9f4ece629b8
MLopez-Ibanez <2620021+MLopez-Ibanez@users.noreply.github.com>
2019-09-21 20:31:40 +0100

    travis_deploy.sh: Fix git push

--- test/travis_deploy.sh
+++ test/travis_deploy.sh
@@ -4,7 +4,7 @@ set -e
 MESSAGE=$(git log --format=%B -n 1 $TRAVIS_COMMIT)
 git config --global user.name "${GIT_NAME}"
 git config --global user.email "${GIT_EMAIL}"
-git checkout -b master
+git checkout master
 git add testbib.pdf
 git commit --message "testbib.pdf: Regenerate (build: $TRAVIS_BUILD_NUMBER)."
 git remote add origin-travis "https://${GH_TOKEN}@${GH_REPO}" > /dev/null 2>&1
