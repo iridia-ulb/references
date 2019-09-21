6615247a4c84fa964700c7fed645f329a87a5ce2
MLopez-Ibanez <2620021+MLopez-Ibanez@users.noreply.github.com>
2019-09-21 18:13:08 +0100

    test/travis_deploy.sh: Fix path.

--- test/travis_deploy.sh
+++ test/travis_deploy.sh
@@ -3,7 +3,7 @@ MESSAGE=$(git log --format=%B -n 1 $TRAVIS_COMMIT)
 #git config user.email "travis@travis-ci.com"
 #git config user.name "Travis CI"
 #git checkout -b master
-git add test/testbib.pdf
+git add testbib.pdf
 git commit --message "testbib.pdf: $MESSAGE $TRAVIS_BUILD_NUMBER"
 #git remote add origin-pages https://${GH_TOKEN}@github.com/MVSE-outreach/resources.git > /dev/null 2>&1
 #git push --quiet --set-upstream origin gh-pages
