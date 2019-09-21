8ae0733a345663aba35dfafeec8210b70fee92ab
MLopez-Ibanez <2620021+MLopez-Ibanez@users.noreply.github.com>
2019-09-21 20:13:25 +0100

    travis_deploy.sh: Fix git status

--- test/travis_deploy.sh
+++ test/travis_deploy.sh
@@ -6,7 +6,7 @@ git config --global user.name "${GIT_NAME}"
 git config --global user.email "${GIT_EMAIL}"
 #git checkout -b master
 git add testbib.pdf
-git st
+git status
 git commit --message "testbib.pdf: Regenerate (build: $TRAVIS_BUILD_NUMBER)."
 #git remote add origin-pages https://${GH_TOKEN}@github.com/MVSE-outreach/resources.git > /dev/null 2>&1
 #git push --quiet --set-upstream origin gh-pages
