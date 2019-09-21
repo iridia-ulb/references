1025d3bafdff746a6f5c9679cb8e62af4d5d91d4
MLopez-Ibanez <2620021+MLopez-Ibanez@users.noreply.github.com>
2019-09-21 20:43:38 +0100

    travis_deploy.sh: avoid infinite loop

--- test/travis_deploy.sh
+++ test/travis_deploy.sh
@@ -6,6 +6,6 @@ git config --global user.name "${GIT_NAME}"
 git config --global user.email "${GIT_EMAIL}"
 git checkout master
 git add testbib.pdf
-git commit --message "testbib.pdf: Regenerate (build: $TRAVIS_BUILD_NUMBER)."
+git commit --message "testbib.pdf: Regenerate (build: $TRAVIS_BUILD_NUMBER) [skip ci]."
 git remote add origin-travis "https://${GH_TOKEN}@${GH_REPO}" > /dev/null 2>&1
 git push  --quiet --set-upstream origin-travis
