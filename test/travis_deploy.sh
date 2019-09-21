cb0628af6321d37589a2f63de50e0a1a89682d62
MLopez-Ibanez <2620021+MLopez-Ibanez@users.noreply.github.com>
2019-09-21 20:07:14 +0100

    try to fix travis deploy

--- test/travis_deploy.sh
+++ test/travis_deploy.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
+set -u
+set -e
 MESSAGE=$(git log --format=%B -n 1 $TRAVIS_COMMIT)
-git config --global user.email "${GIT_EMAIL}"
 git config --global user.name "${GIT_USER}"
+git config --global user.email "${GIT_EMAIL}"
 #git checkout -b master
 git add testbib.pdf
-git commit --message "testbib.pdf: $MESSAGE"
+git st
+git commit --message "testbib.pdf: Regenerate."
 #git remote add origin-pages https://${GH_TOKEN}@github.com/MVSE-outreach/resources.git > /dev/null 2>&1
 #git push --quiet --set-upstream origin gh-pages
 git push "https://${GH_TOKEN}@${GH_REPO}" master
+git config --global --list
