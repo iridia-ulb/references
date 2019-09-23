0eea051805fc165c65e50164ca21a4e79351c0d1
MLopez-Ibanez <2620021+MLopez-Ibanez@users.noreply.github.com>
2019-09-23 14:20:16 +0100

    Move travis git env vars to .travis.yml

--- test/travis_deploy.sh
+++ test/travis_deploy.sh
@@ -2,8 +2,8 @@
 set -u
 set -e
 MESSAGE=$(git log --format=%B -n 1 $TRAVIS_COMMIT)
-git config --global user.name "iridiaservice"
-git config --global user.email "social@iridia.ulb.ac.be"
+git config --global user.name "${GIT_NAME}"
+git config --global user.email "${GIT_EMAIL}"
 git checkout master
 git add testbib.pdf
 git commit --message "testbib.pdf: Regenerate (build: $TRAVIS_BUILD_NUMBER) [skip ci]."
