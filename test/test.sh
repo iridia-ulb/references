3dfc6672246da79d17c0e374b82a8857e016f28e
MLopez-Ibanez <2620021+MLopez-Ibanez@users.noreply.github.com>
2019-09-29 15:24:47 +0100

    Use test.sh for travis and check for bibtex warnings.
     * .travis.yml: Use test.sh
     * test/test.sh: check for bibtex warnings.

--- test/test.sh
+++ test/test.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
-latexmk -halt-on-error -g --pdf testbib.tex
+latexmk -halt-on-error -g --pdf testbib.tex | tee /dev/stderr | grep -F "Warning--" --quiet
+if [ $? -eq 0 ]; then
+    echo "Error: Please fix bibtex Warnings"
+    exit 1
+fi
+exit 0
