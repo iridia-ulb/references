4b0d4c094f60a9e5384b18ab69418479bba2db02
MLopez-Ibanez <2620021+MLopez-Ibanez@users.noreply.github.com>
2019-09-29 16:58:59 +0100

    Fix travis_test.sh warning check

--- test/travis_test.sh
+++ test/travis_test.sh
@@ -1,7 +1,16 @@
-#!/bin/sh
-latexmk -halt-on-error -g --pdf testbib.tex | tee /dev/stderr | grep -F "Warning--" --quiet
-if [ $? -eq 0 ]; then
-    echo "Error: Please fix bibtex Warnings"
+#!/bin/bash
+set -u
+set -o pipefail
+latexmk -halt-on-error -interaction=nonstopmode -gg --pdf testbib.tex | tee .bibtex-warnings
+if [ $? -ne 0 ]; then
+    echo "Error: latexmk failed"
     exit 1
 fi
+grep --quiet "Warning--" .bibtex-warnings
+if [ $? -eq 0 ]; then
+    echo "Error: Please fix bibtex Warnings:"
+    grep "Warning--" .bibtex-warnings
+    exit 1
+fi
+echo "No bibtex warnings! Good job!"
 exit 0
