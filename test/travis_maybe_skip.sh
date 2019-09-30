2339b8bc3305e3aedf3caf2dd90ed0d891bbed03
MLopez-Ibanez <2620021+MLopez-Ibanez@users.noreply.github.com>
2019-09-30 12:27:57 +0100

    * test/travis_maybe_skip.sh: Use -m so that merge commits also triger a build.

--- test/travis_maybe_skip.sh
+++ test/travis_maybe_skip.sh
@@ -3,7 +3,8 @@ set -u
 
 INCLUDE_ONLY="\.bib\|\.tex\|testbib\.best\|travis"
 
-git diff-tree --no-commit-id --name-only -r ${TRAVIS_COMMIT} | grep -e ${INCLUDE_ONLY} --quiet
+# Use -m so that merge commits are also considered
+git diff-tree -m  --no-commit-id --name-only -r ${TRAVIS_COMMIT} | grep -e ${INCLUDE_ONLY} --quiet
 if [ $? -ne 0 ]; then
     travis_terminate 0
     exit 1
