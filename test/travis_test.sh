ebec8276b8514194b610ff7ffe9b0fb202760aab
MLopez-Ibanez <2620021+MLopez-Ibanez@users.noreply.github.com>
2019-10-22 08:53:57 +0100

    * biblio.bib: Fix wrong doi.  * test/travis_test.sh: Check doi.

--- test/travis_test.sh
+++ test/travis_test.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 set -u
 set -o pipefail
+
+grep --quiet -e "doi[[:space:]]*=.\+http" ../*.bib
+if [ $? -eq 0 ]; then
+    echo "Error: the doi field should not be an URL"
+    grep -n -e "doi[[:space:]]*=.\+http" ../*.bib
+    exit 1
+fi
+
 latexmk -halt-on-error -interaction=nonstopmode -gg --pdf testbib.tex | tee .bibtex-warnings
 if [ $? -ne 0 ]; then
     echo "Error: latexmk failed"
