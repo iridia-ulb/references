d9233b1a00bfc3993ff9a29bd8b1066e305cfd04
MLopez-Ibanez <2620021+MLopez-Ibanez@users.noreply.github.com>
2019-11-03 13:54:59 +0000

    * scripts/optbib2html.sh: rename html files.

--- scripts/optbib2html.sh
+++ scripts/optbib2html.sh
@@ -14,5 +14,8 @@ bib2bib --warn-error --expand --expand-xrefs authors.bib abbrev.bib journals.bib
 
 bibtex2html --title "IRIDIA BibTeX Repository" --html-entities --named-field url http --named-field springerlink Springer --named-field supplement "supplementary material" --named-field pdf PDF -dl -u -s bibstyles/plainweb -macros-from macros.tex -o index -citefile $tmpcitefile $tmpbib
 
+# We need to do this so it will not get overwritten when switching branches
+mv index.html references.html
+mv index_bib.html references_bib.html
 # remove temporary files
 rm -f bib2html*
