eeda91a2e4e77e15498ffc6b3ce512c75ee519d2
MLopez-Ibanez <2620021+MLopez-Ibanez@users.noreply.github.com>
2019-11-03 15:13:29 +0000

    test/travis_deploy.sh: Remove debug output.

--- test/travis_deploy.sh
+++ test/travis_deploy.sh
@@ -4,11 +4,14 @@ set -e
 MESSAGE=$(git log --format=%B -n 1 $TRAVIS_COMMIT)
 git config --global user.name "${GIT_NAME}"
 git config --global user.email "${GIT_EMAIL}"
-ls
-echo "$(pwd)"
-git status
 git checkout master
-git add test/testbib.pdf
+git add -f test/testbib.pdf
+# If there are no changes to the compiled out (e.g. this is a README update) then just bail.
+if git diff --cached --quiet; then
+    echo "No changes to the output on this push; exiting."
+    exit 0
+fi
+
 git commit --message "testbib.pdf: Regenerate (build: $TRAVIS_BUILD_NUMBER) [skip ci]."
 git remote add origin-travis "https://${GH_TOKEN}@${GH_REPO}" > /dev/null 2>&1
 git push  --quiet --set-upstream origin-travis
