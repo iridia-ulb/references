1058004478e673471cbb3e984c6239d675462332
MLopez-Ibanez <2620021+MLopez-Ibanez@users.noreply.github.com>
2019-11-04 21:22:11 +0000

    * scripts/optbib2html.sh: acmdl links don't work.

--- scripts/optbib2html.sh
+++ scripts/optbib2html.sh
@@ -10,7 +10,7 @@ tmpcitefile=$(mktemp --tmpdir citefileXXXXXXXXXX)
 bib2bib --warn-error --expand --expand-xrefs authors.bib abbrev.bib journals.bib biblio.bib crossref.bib -ob $tmpbib -oc $tmpcitefile
 
 cd web
-bibtex2html --nodoc --html-entities -linebreak -css bibtex.css --named-field url http --named-field springerlink Springer --named-field supplement "supplementary material" --named-field acmdl http --named-field pdf pdf -dl -u -s ../bibstyles/plainweb -macros-from ../macros.tex -o index -citefile $tmpcitefile $tmpbib
+bibtex2html --nodoc --html-entities -linebreak -css bibtex.css --named-field url http --named-field springerlink Springer --named-field supplement "supplementary material"  --named-field pdf pdf -dl -u -s ../bibstyles/plainweb -macros-from ../macros.tex -o index -citefile $tmpcitefile $tmpbib
 
 cat header.htm index.html footer.htm > tmp.html
 mv tmp.html index.html
