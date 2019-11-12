27d28d422f536cf1fcae3bc2273f595441300bf5
MLopez-Ibanez <2620021+MLopez-Ibanez@users.noreply.github.com>
2019-11-12 23:36:50 +0000

    * test/travis_maybe_skip.sh: consider range include in push.

--- test/travis_maybe_skip.sh
+++ test/travis_maybe_skip.sh
@@ -4,7 +4,7 @@ set -u
 INCLUDE_ONLY="\.bib\|\.tex\|travis\|optbib2html\|deploy"
 
 # Use -m so that merge commits are also considered
-git diff-tree -m  --no-commit-id --name-only -r ${TRAVIS_COMMIT} | grep -e ${INCLUDE_ONLY} --quiet
+git diff-tree -m  --no-commit-id --name-only -r ${TRAVIS_COMMIT_RANGE} | grep -e ${INCLUDE_ONLY} --quiet
 if [ $? -ne 0 ]; then
     travis_terminate 0
     exit 1
