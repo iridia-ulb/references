d40b23497778eb1cf6ae769d2c8cf1f6ef7866ab
MLopez-Ibanez <2620021+MLopez-Ibanez@users.noreply.github.com>
2019-11-13 00:04:12 +0000

    * test/travis_maybe_skip.sh: debug commit range

--- .travis/travis_maybe_skip.sh
+++ .travis/travis_maybe_skip.sh
@@ -4,10 +4,9 @@ set -u
 INCLUDE_ONLY="\.bib\|\.tex\|travis\|optbib2html\|deploy"
 
 # Use -m so that merge commits are also considered
-echo ${TRAVIS_COMMIT_RANGE}
-echo ${TRAVIS_COMMIT}
-git diff-tree -m  --no-commit-id --name-only -r ${TRAVIS_COMMIT_RANGE}
-git diff-tree -m  --no-commit-id --name-only -r ${TRAVIS_COMMIT_RANGE} | grep -e ${INCLUDE_ONLY} --quiet
+echo git diff --raw -c --no-commit-id --name-only ${TRAVIS_COMMIT_RANGE}
+git diff --raw -c --no-commit-id --name-only ${TRAVIS_COMMIT_RANGE}
+git diff --raw -c --no-commit-id --name-only ${TRAVIS_COMMIT_RANGE} | grep -e ${INCLUDE_ONLY} --quiet
 if [ $? -ne 0 ]; then
     travis_terminate 0
     exit 1
