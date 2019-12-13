cfc8fbd8edc923557ed7f77ecf2a963aaf610078
MLopez-Ibanez <2620021+MLopez-Ibanez@users.noreply.github.com>
2019-12-13 22:52:24 +0000

    .travis/texlive/texlive_install.sh: Try to reset install.

--- .travis/texlive/texlive_install.sh
+++ .travis/texlive/texlive_install.sh
@@ -3,17 +3,17 @@
 # Originally from https://github.com/latex3/latex3
 
 export PATH=/tmp/texlive/bin/x86_64-linux:$PATH
-if ! command -v texlua > /dev/null; then
+#if ! command -v texlua > /dev/null; then
   # Obtain TeX Live
   wget http://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz
   tar -xzf install-tl-unx.tar.gz
   cd install-tl-20*
 
   # Install a minimal system
-  ./install-tl --profile=../support/texlive.profile
+  ./install-tl --profile=../.travis/texlive/texlive.profile
 
   cd ..
-fi
+#fi
 
 # Update tlmgr itself
 tlmgr update --self
@@ -29,5 +29,3 @@ tlmgr option -- autobackup 0
 
 # Update the TL install but add nothing new
 tlmgr update --self --all --no-auto-install
-
-
