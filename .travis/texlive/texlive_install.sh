de46cc7ed0d85ce08c110606ceb92add41e37be9
MLopez-Ibanez <2620021+MLopez-Ibanez@users.noreply.github.com>
2020-02-07 16:25:18 +0000

    .travis/: Try to fix texlive install.

--- .travis/texlive/texlive_install.sh
+++ .travis/texlive/texlive_install.sh
@@ -8,21 +8,21 @@ if ! command -v tlmgr > /dev/null; then
   wget http://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz
   tar -xzf install-tl-unx.tar.gz
   cd install-tl-20*
-
   # Install a minimal system
   ./install-tl --profile=../.travis/texlive/texlive.profile
-
   cd ..
 fi
+
+# Keep no backups (not required, simply makes cache bigger)
+tlmgr option -- autobackup 0
 
 # Update tlmgr itself
 tlmgr update --self
 
+tlmgr init-usertree
+
 # We specify the directory in which it is located texlive_packages
 tlmgr install $(sed 's/\s*#.*//;/^\s*$/d' .travis/texlive/texlive_packages)
-
-# Keep no backups (not required, simply makes cache bigger)
-tlmgr option -- autobackup 0
 
 # Update the TL install but add nothing new
 tlmgr update --self --all --no-auto-install
--- .travis/travis_maybe_skip.sh
+++ .travis/travis_maybe_skip.sh
@@ -5,7 +5,6 @@ INCLUDE_ONLY="\.bib\|\.tex\|travis\|optbib2html\|deploy"
 
 # Use -m so that merge commits are also considered
 echo git diff --raw -c --no-commit-id --name-only ${TRAVIS_COMMIT_RANGE}
-git diff --raw -c --no-commit-id --name-only ${TRAVIS_COMMIT_RANGE}
 git diff --raw -c --no-commit-id --name-only ${TRAVIS_COMMIT_RANGE} | grep -e ${INCLUDE_ONLY} --quiet
 if [ $? -ne 0 ]; then
     exit 1
