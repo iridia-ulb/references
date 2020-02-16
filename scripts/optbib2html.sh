39d62da3c1299b9560ac68a369240208e7680734
MLopez-Ibanez <2620021+MLopez-Ibanez@users.noreply.github.com>
2020-02-16 16:49:04 +0000

    * biblio.bib: Fix annotations.  * scripts/optbib2html.sh: Print annotations.  * web/bibtex.css: Show annotations, keywords and abstract in smaller font.

--- scripts/optbib2html.sh
+++ scripts/optbib2html.sh
@@ -12,17 +12,17 @@ TIDYCONF=$(realpath scripts/tidy.conf)
 bib2bib --warn-error --expand --expand-xrefs $BIBFILES -ob $tmpbib -oc $tmpcitefile
 
 cd web
-bibtex2html --nodoc --html-entities -linebreak -css bibtex.css --named-field url http --named-field springerlink Springer --named-field supplement "supplementary material"  --named-field pdf pdf -dl -u -s ../bibstyles/plainweb -macros-from ../macros.tex -o index -citefile $tmpcitefile $tmpbib
+bibtex2html --nodoc --html-entities -linebreak -css bibtex.css --named-field url http --named-field springerlink Springer --named-field supplement "supplementary material" --named-field pdf pdf --note annote -dl -u -s ../bibstyles/plainweb -macros-from ../macros.tex -o index -citefile $tmpcitefile $tmpbib
 
 cat header.htm index.html footer.htm > tmp.html
 mv tmp.html index.html
 # Add clickable anchors
 sed -i 's/<a name="\([^"]\+\)">/<a href="#\1" name="\1">/g' index.html
-tidy -config $TIDYCONF -m index.html
+#tidy -config $TIDYCONF -m index.html
 
 cat header.htm index_bib.html footer.htm > tmp.html
 mv tmp.html index_bib.html
-tidy -config $TIDYCONF -m index_bib.html
+#tidy -config $TIDYCONF -m index_bib.html
 
 # remove temporary files
 rm -f bib2html*
