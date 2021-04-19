faa7f050664c1fe24db2f21a42ff08124aa2fac0
MLopez-Ibanez <2620021+MLopez-Ibanez@users.noreply.github.com>
2021-04-19 19:38:23 +0100

    * .travis/travis_deploy.sh: Revert previous commit.

--- .travis/travis_deploy.sh
+++ .travis/travis_deploy.sh
@@ -4,6 +4,7 @@ set -e
 MESSAGE=$(git log --format=%B -n 1 $TRAVIS_COMMIT)
 git config --global user.name "${GIT_NAME}"
 git config --global user.email "${GIT_EMAIL}"
+git checkout master
 git add -f test/testbib.pdf test/testshortbib.pdf
 # If there are no changes to the compiled out (e.g. this is a README update) then just bail.
 if git diff --cached --quiet; then
@@ -13,5 +14,5 @@ fi
 
 git commit --message "testbib.pdf: Regenerate (build: $TRAVIS_BUILD_NUMBER) [skip ci]."
 git remote add origin-travis "https://${GH_TOKEN}@${GH_REPO}" > /dev/null 2>&1
-git push  --quiet --set-upstream origin-travis
+git push  --quiet --set-upstream origin-travis 
 
