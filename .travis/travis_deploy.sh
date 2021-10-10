6fac2cc9330101bd0480eedfeaa6ee1afc1284a6
MLopez-Ibanez <2620021+MLopez-Ibanez@users.noreply.github.com>
2021-10-10 15:13:20 +0200

    Fix travis_deploy.sh

--- .travis/travis_deploy.sh
+++ .travis/travis_deploy.sh
@@ -4,7 +4,8 @@ set -e
 MESSAGE=$(git log --format=%B -n 1 $TRAVIS_COMMIT)
 git config --global user.name "${GIT_NAME}"
 git config --global user.email "${GIT_EMAIL}"
-git checkout gh-pages
+git remote add origin-travis "https://${GH_TOKEN}@${GH_REPO}" > /dev/null 2>&1
+git checkout -b gh-pages --track origin-travis/gh-pages 
 mv --force -t . test/testbib.pdf test/testshortbib.pdf
 git add -f testbib.pdf testshortbib.pdf
 # If there are no changes to the compiled out (e.g. this is a README update) then just bail.
@@ -14,6 +15,5 @@ if git diff --cached --quiet; then
 fi
 
 git commit --message "testbib.pdf: Regenerate (build: $TRAVIS_BUILD_NUMBER) [skip ci]."
-git remote add origin-travis "https://${GH_TOKEN}@${GH_REPO}" > /dev/null 2>&1
 git push  --quiet --set-upstream origin-travis 
 
