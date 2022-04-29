db4a955ce51b135e641fc5ea3e6d52654f62c33f
MLopez-Ibanez <2620021+MLopez-Ibanez@users.noreply.github.com>
2022-04-29 16:43:52 +0200

    * setup_worktree.sh: New.

---
+++ setup_worktree.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+if [ "$#" -ne 2 ] || ! [ -d "$2" ]; then
+  echo "Usage: $0 BRANCH_NAME DIRECTORY" >&2
+  exit 1
+fi
+if [ ! -r "./biblio.bib" ]; then
+  echo "This command must be run from the root of https://github.com/iridia-ulb/references" >&2
+  exit 1
+fi
+# Exit when any command fails
+set -e
+BRANCH="$1"
+DIR="$2"
+git worktree add -B "$BRANCH" "$DIR/bib"
+pushd "$DIR"
+echo "bib/" >> .gitignore
+cp bib/test/.latexmkrc .
+git add -f .gitignore .latexmkrc bib/README.md bib/*.bib
+git ci -a -m "Setup https://github.com/iridia-ulb/references"
+git push
+popd
+echo "$0: Please add \bibliography{bib/abbrev,bib/journals,bib/authors,bib/biblio,bib/crossref} to the main.tex file"
+echo "$0: All done!"
+exit 0
