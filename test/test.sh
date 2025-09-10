9f4d24da96c14b2dd62065a07b00161f5c31fb06
MLopez-Ibanez <2620021+MLopez-Ibanez@users.noreply.github.com>
2025-09-10 17:26:17 +0100

    * test/test.sh: Accept -single.

--- test/test.sh
+++ test/test.sh
@@ -10,38 +10,30 @@ fi
 
 ./fast_check.sh ../*.bib || exit 1
 
-travis_fold_start() {
-    if [ ! -z ${TRAVIS:-} ]; then
-        echo -e "travis_fold:start: $1\033[33;1m$2\033[0m"
-    else
-        echo -e "::group:: $1 \033[33;1m$2\033[0m"
-    fi
+fold_start() {
+    echo -e "::group:: $1 \033[33;1m$2\033[0m"
 }
 
-travis_fold_end() {
-    if [ ! -z ${TRAVIS:-} ]; then
-        echo -e "\ntravis_fold:end: $1\r"
-    else
-        echo -e "\n::endgroup::\n"
-    fi
+fold_end() {
+    echo -e "\n::endgroup::\n"
 }
 
 latexmake() {
     TEXMAIN=$1
     BST=$2
-    travis_fold_start latexmk.1 "latexmk $TEXMAIN $BST"
+    fold_start latexmk.1 "latexmk $TEXMAIN $BST"
     rm -f *.bbl *.aux *.log *.out *.bcf *.blg *.fls *.hd *.fdb_latexmk *.run.xml *.syctex.gz
 
     if [ -h tmp.bst ]; then
         rm -f tmp.bst
     fi
-    if [ $bst != "" ]; then
+    if [ "$BST" != "" ]; then
         ln -s ${BST}.bst tmp.bst
     fi
     
     latexmk -silent -halt-on-error -interaction=nonstopmode -gg --pdf $TEXMAIN | tee .bibtex-warnings
     if [ $? -ne 0 ]; then
-        travis_fold_end latexmk.1
+        fold_end latexmk.1
         echo "Error: latexmk failed"
         LOGFILE=${TEXMAIN%.*}.log
         cat "$LOGFILE"
@@ -50,7 +42,7 @@ latexmake() {
 
     grep --quiet "Warning--" .bibtex-warnings
     if [ $? -eq 0 ]; then
-        travis_fold_end latexmk.1
+        fold_end latexmk.1
         echo "Error: Please fix bibtex Warnings:"
         grep "Warning--" .bibtex-warnings
         exit 1
@@ -58,28 +50,34 @@ latexmake() {
     
     grep --quiet "WARN" .bibtex-warnings
     if [ $? -eq 0 ]; then
-        travis_fold_end latexmk.1
+        fold_end latexmk.1
         echo "Error: Please fix biblatex Warnings:"
         grep "WARN" .bibtex-warnings
         exit 1
     fi
-    travis_fold_end latexmk.1
+    fold_end latexmk.1
 }
 
 TEXMAIN="testbib.tex"
 # FIXME: This doesn't seem to do anything useful.
-# travis_fold_start texliveonfly.1 "texliveonfly $TEXMAIN"
+# fold_start texliveonfly.1 "texliveonfly $TEXMAIN"
 # texliveonfly $TEXMAIN
-# travis_fold_end texliveonfly.1
+# fold_end texliveonfly.1
 
-for main in "testbib" "testshortbib"; do
-    # FIXME: Too many warnings
-    # "../bibstyles/ACM-Reference-Format" 
-    for bst in "../bibstyles/splncs04abbrev" "../bibstyles/abbrvnatamp" "$main"; do
-        latexmake "${main}.tex" "$bst"
+# Support single test mode
+if [ "${1:-}" = "-single" ]; then
+    TEXMAIN="$2"
+    BST="${3:-}"
+    latexmake "$TEXMAIN" "$BST"
+else
+    for main in "testbib" "testshortbib"; do
+        # FIXME: Too many warnings
+        # "../bibstyles/ACM-Reference-Format" 
+        for bst in "../bibstyles/splncs04abbrev" "../bibstyles/abbrvnatamp" "$main"; do
+            latexmake "${main}.tex" "$bst"
+        done
     done
-done
-latexmake "testbiblatex.tex" ""
-
+    latexmake "testbiblatex.tex" ""
+fi
 echo "No bibtex warnings! Good job!"
 exit 0
